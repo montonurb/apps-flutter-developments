dccfe6a1647fe1c07a04b42e8cfded26ee3512d9
montonurb <brunomoncar007@gmail.com>
2020-01-06 10:53:31 -0300

    Conditional if-else

---
+++ dart-exercises/conditional.dart
@@ -0,0 +1,12 @@
+main(){
+  var x = 20;
+  var y = 16;
+
+  if(x + y == 30){
+    print("Soma está errada!");
+  } else if (x + y == 35){
+    print("Soma correta");
+  } else {
+    print("Soma errada 2!");
+  }
+}
