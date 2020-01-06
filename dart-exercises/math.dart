041558cfe16e6d88a07baf0fa1401c1394f4c9ce
montonurb <brunomoncar007@gmail.com>
2020-01-05 22:42:11 -0300

    mathmatic operations

---
+++ dart-exercises/math.dart
@@ -0,0 +1,13 @@
+main() {
+  var som = 6 + 3;
+  var sub = 8 - 2;
+  var mul = 2 * 5;
+  var div = 10 / 2;
+  var res = 10 % 3;
+
+  print(som);
+  print(sub);
+  print(mul);
+  print(div);
+  print(res);
+}
