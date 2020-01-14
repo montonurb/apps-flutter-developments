1f0752a3d8d8ecd59a68e49b1a28cc42969f7fd9
montonurb <brunomoncar007@gmail.com>
2020-01-14 20:44:54 -0300

    funcao map, mas ao cadastrar um novo, esta substituindo, ao inves de adicionar

---
+++ dart-exercises/maps.dart
@@ -0,0 +1,47 @@
+import "dart:io";
+
+Map<String, dynamic> cadastro = {};
+
+main() {
+  //Map<tipo da variavel, tipo do valor> variavel = {}
+/*   Map<String, dynamic> maps = {
+    "nome": "Bruno Monteiro",
+    "idade": 23,
+    "cidade": "Parnaíba",
+    "estado": "Piauí",
+  };
+  print(maps["nome"]);
+  maps["nome"] = "Bruno Monteiro Cardoso";
+  print(maps["nome"]);
+  print(maps); */
+  bool condicao = true;
+
+  while (condicao) {
+    print("Informe um comando:");
+    String comando = stdin.readLineSync();
+    if (comando == "sair") {
+      print("=== Programa finalizado! ===");
+      condicao = false;
+    } else if (comando == "cadastrar") {
+      cadastrar();
+    } else if (comando == "imprimir") {
+      print(cadastro);
+    } else {
+      print("=== Comando invalido! ===");
+    }
+  }
+}
+
+cadastrar() {
+  print("Informe o seu nome:");
+  cadastro["nome"] = stdin.readLineSync();
+
+  print("Informe a sua idade:");
+  cadastro["idade"] = stdin.readLineSync();
+
+  print("Informe a sua cidade:");
+  cadastro["cidade"] = stdin.readLineSync();
+
+  print("Informe o seu estado:");
+  cadastro["estado"] = stdin.readLineSync();
+}
