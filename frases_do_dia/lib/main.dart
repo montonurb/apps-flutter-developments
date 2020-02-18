35efcd5090b918fb2347a48d915dbdb6b1a995ad
montonurb <brunomoncar007@gmail.com>
2020-02-17 23:10:57 -0300

    finalizando a aula de formatação de texto

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,12 +4,27 @@ void main(){
 
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Row(children: <Widget>[
-      Text("texto 1: "),
-      Text("texto 2: "),
-      Text("t3")
-    ],),
-    //home: Container(color: Colors.white,),
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Lorem ipsum ",
+            style: TextStyle(
+              fontSize: 35,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.normal, //ao invés de usar normal, posso colocar w100, para definir
+              letterSpacing: 0,
+              wordSpacing: 0,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.greenAccent,
+              decorationStyle: TextDecorationStyle.solid,
+              color: Colors.black
+            ),
+          )
+        ],
+      ),
+    ),
   ));
 
 }
