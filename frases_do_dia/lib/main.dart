d7dbb0ce637d150e47dd3f29956fe3ec00f3dad8
montonurb <brunomoncar007@gmail.com>
2020-02-26 16:19:47 -0300

    Stateful e Stateless

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,9 +4,54 @@ void main(){
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false, //Para tirar a faixa de Debug da aplicação
-    home: Scaffold(
+    home: HomeStateful() ,
+  ));
+
+}
+
+class HomeStateful extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<HomeStateful> {
+  var _text = "Bruno Monteiro";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Instagram"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: (){
+                setState(() {
+                  _text = "Clicou com sucesso!";
+                });
+              },
+              //color: Colors.amber, //caso eu queira mudar a cor do botão
+              child: Text("Clique aqui"),
+            ),
+            Text("Nome: $_text")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Instagram";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titulo),
         backgroundColor: Colors.green,
       ),
       body: Padding(
@@ -26,7 +71,6 @@ void main(){
           ),
         ),
       ),
-    ) ,
-  ));
-
+    );
+  }
 }
