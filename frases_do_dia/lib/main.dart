364671ca5746dc9e3934d043017775728aeb658e
montonurb <brunomoncar007@gmail.com>
2020-02-26 17:06:58 -0300

    Interface do aplicativo Frases do Dia

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,75 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false, //Para tirar a faixa de Debug da aplicação
-    home: HomeStateful() ,
+    debugShowCheckedModeBanner: false,
+    home: HomeStateful(),
   ));
-
 }
 
 class HomeStateful extends StatefulWidget {
   @override
-  _State createState() => _State();
+  _HomeStatefulState createState() => _HomeStatefulState();
 }
 
-class _State extends State<HomeStateful> {
-  var _text = "Bruno Monteiro";
+class _HomeStatefulState extends State<HomeStateful> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text("Frases do Dia"),
         backgroundColor: Colors.green,
       ),
-      body: Container(
+      body: Center(
+        child: Container(
+        padding: EdgeInsets.all(16),
+        //width: double.infinity,
+        /* decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.amber)
+        ), */
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              onPressed: (){
-                setState(() {
-                  _text = "Clicou com sucesso!";
-                });
-              },
-              //color: Colors.amber, //caso eu queira mudar a cor do botão
-              child: Text("Clique aqui"),
+            Image.asset("images/logo.png"),
+            Text(
+              "Clique abaixo para gerar uma frase!",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 25,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
             ),
-            Text("Nome: $_text")
+            RaisedButton(
+              onPressed: (){},
+              color: Colors.green,
+              child: Text(
+                "Nova Frase",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            )
           ],
         ),
       ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    var _titulo = "Instagram";
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_titulo),
-        backgroundColor: Colors.green,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Text("Sua foto aqui."),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.lightGreen,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: <Widget>[
-              Text("Texto 1."),
-              Text("Texto 2."),
-              Text("Texto 2.")
-            ]
-          ),
-        ),
       ),
     );
   }
