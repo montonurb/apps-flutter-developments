d90d2aa3bf376fd93ceb62585a8462f9d81025ac
montonurb <brunomoncar007@gmail.com>
2020-02-27 10:17:48 -0300

    Finalizando aplicação Frases do Dia

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -13,6 +14,24 @@ class HomeStateful extends StatefulWidget {
 }
 
 class _HomeStatefulState extends State<HomeStateful> {
+
+  var _frases = [
+    "Sou apenas um pequeno planeta que se perde diariamente em todo o seu universo.",
+    "Novas amizades serão sempre bem-vindas para darem cor e alegria ao meu dia.",
+    "Gratidão não é pagamento, mas um reconhecimento que se demonstra no dia",
+    "Nem toda mudança importante acontece de repente e faz barulho, algumas"
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase(){
+
+    var randNumber = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[randNumber];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +52,7 @@ class _HomeStatefulState extends State<HomeStateful> {
           children: <Widget>[
             Image.asset("images/logo.png"),
             Text(
-              "Clique abaixo para gerar uma frase!",
+              _fraseGerada,
               textAlign: TextAlign.justify,
               style: TextStyle(
                 fontSize: 25,
@@ -42,7 +61,7 @@ class _HomeStatefulState extends State<HomeStateful> {
               ),
             ),
             RaisedButton(
-              onPressed: (){},
+              onPressed: _gerarFrase,
               color: Colors.green,
               child: Text(
                 "Nova Frase",
