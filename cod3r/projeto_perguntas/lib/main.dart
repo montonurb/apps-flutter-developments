7d6f0053d3c434d84c37f10c7d68d025706c451d
montonurb <brunomoncar007@gmail.com>
2020-04-03 17:36:02 -0300

    transformando algumas classes e variáveis em privados

--- cod3r/projeto_perguntas/lib/main.dart
+++ cod3r/projeto_perguntas/lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-  void responder(){
+  void _responder(){
     setState(() {
-      perguntaSelecionada++;
+      _perguntaSelecionada++;
     });
-    print(perguntaSelecionada);
+    print(_perguntaSelecionada);
   }
 
   @override
@@ -27,18 +27,18 @@ class PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[perguntaSelecionada]),
+            Text(perguntas[_perguntaSelecionada]),
             RaisedButton(
               child: Text('Valor 1'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             RaisedButton(
               child: Text('Valor 2'),
-              onPressed: responder
+              onPressed: _responder
             ),
             RaisedButton(
               child: Text('Valor 3'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
           ],
         ),
@@ -49,7 +49,7 @@ class PerguntaAppState extends State<PerguntaApp> {
 
 class PerguntaApp extends StatefulWidget {
   
-  PerguntaAppState createState() {
-    return PerguntaAppState();
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
 }
