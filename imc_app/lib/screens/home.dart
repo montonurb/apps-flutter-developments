c6121040db69496dee440fa90d6c3d82ae9212d0
Bruno Monteiro <brunomoncar007@gmail.com>
2020-05-23 01:24:11 -0300

    adicionando tla de DashBoard

--- imc_app/lib/screens/home.dart
+++ imc_app/lib/screens/home.dart
@@ -1,21 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:imc_app/screens/formulario.dart';
+import 'package:imc_app/screens/imc_list.dart';
+import 'package:imc_app/screens/login.dart';
+import 'package:imc_app/screens/menu_drawer.dart';
+import 'package:imc_app/screens/settings.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Tela Inicial'),
-      ),
-      body: Flexible(
-        child: GridView.count(
-          childAspectRatio: 1.0,
+        drawer: MenuDrawer(),
+        appBar: AppBar(title: Text('Tela Inicial')),
+        body: Container(
+            child: GridView.count(
+          padding: EdgeInsets.all(20),
           crossAxisCount: 2,
           children: <Widget>[
-            
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return FormularioIMC();
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF26A69A),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.star_border,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Iniciar Avaliação',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ImcList();
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF26A69A),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.star_border,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Lista de Avaliações',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ConfiguracoesApp();
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF26A69A),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.settings,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Configurações',
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Login();
+                }));
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF26A69A),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.exit_to_app,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Sair',
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
-        )
-      ),
-    );
+        )));
   }
 }
