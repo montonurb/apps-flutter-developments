5920786c2d82e4c217618ebc73ecb4dbb743e5b7
Bruno Monteiro <brunomoncar007@gmail.com>
2020-05-26 00:55:27 -0300

    adicionando varivel ID no models

--- imc_app/lib/models/dados_imc.dart
+++ imc_app/lib/models/dados_imc.dart
@@ -1,14 +1,15 @@
 class DadosImc {
+  final int id;
   final double peso;
   final double altura;
-  final DateTime dataAvaliacao;
+  final String dataAvaliacao;
 
   DadosImc(
-    this.peso, this.altura, this.dataAvaliacao
+    this.id, this.peso, this.altura, this.dataAvaliacao
   );
 
   @override
   String toString() {
-    return 'DadosImc{peso: $peso, altura: $altura, data: $dataAvaliacao}';
+    return 'DadosImc{Id: $id, peso: $peso, altura: $altura, data: $dataAvaliacao}';
   }
 }
