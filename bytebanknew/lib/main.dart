9b21de12f087b0ff327bba8ba047b2367c65a60d
Bruno Monteiro <brunomoncar007@gmail.com>
2020-06-08 21:45:47 -0300

    comunicando com http

--- bytebanknew/lib/main.dart
+++ bytebanknew/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:bytebanknew/http/web_client.dart';
 import 'package:bytebanknew/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankApp());
+  findAll().then((transactions) => print('new transactions $transactions'));
 }
 
 class BytebankApp extends StatelessWidget {
