538b68f950a6ceb2be6d0c41154ff58963804f15
Bruno Monteiro <brunomoncar007@gmail.com>
2020-06-11 17:48:44 -0300

    tirando o save e findall de testes

--- bytebanknew/lib/main.dart
+++ bytebanknew/lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:bytebanknew/http/web_client.dart';
 import 'package:bytebanknew/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankApp());
-  findAll().then((transactions) => print('new transactions $transactions'));
+
+  //save(Transaction(200.0, Contact(0, 'Maria Clara', 1003))).then((transaction) => print(transaction));
+  //findAll().then((transactions) => print('new transactions $transactions'));
 }
 
 class BytebankApp extends StatelessWidget {
