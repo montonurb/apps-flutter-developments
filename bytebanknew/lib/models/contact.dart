560b0d58cac49cd009409fb3592c7ec2655605a0
Bruno Monteiro <brunomoncar007@gmail.com>
2020-06-11 17:50:32 -0300

    colocando o método toJson e Map

--- bytebanknew/lib/models/contact.dart
+++ bytebanknew/lib/models/contact.dart
@@ -10,4 +10,16 @@ class Contact {
 
     return 'Contact{id: $id, name: $name, accountNumber: $accountNumber}';
   }
+
+  Contact.fromJson(Map<String, dynamic>json)
+  : id = json['id'],
+    name = json['name'],
+    accountNumber = json['accountNumber'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        'name' : name,
+        'accountNumber' : accountNumber,
+      };
+
 }
